7904f2f09cc9ebe542e295da8c8dd6279fa6191c
Seth Miller <millerado@gmail.com>
2023-01-30 13:10:43 -0700

    added basic components

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Bio from './components/Bio';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import ProfilePicture from './components/ProfilePicture';
+import ProjectDisplay from './components/ProjectDisplay';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <ProfilePicture />
+      <Bio />
+      <ProjectDisplay />
+      <Resume />
+      <Contact />
     </div>
   );
 }
