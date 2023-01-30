f7b9332a205690fc4ef5ba02e96df3517c6c2022
Seth Miller <millerado@gmail.com>
2023-01-30 13:02:11 -0700

    create header componenet

---
+++ portfolio-site/src/components/Header.js
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h2>Seth Miller</h2>
+        <div class='nav-links'>
+          <a href='#bio'>
+            <p>Bio</p>
+          </a>
+          <a href='#projects'>
+            <p>Projects</p>
+          </a>
+          <a href='#resume'>
+            <p>Resume</p>
+          </a>
+          <a href='#contact'>
+            <p>Contact</p>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
